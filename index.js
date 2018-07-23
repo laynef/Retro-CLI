3b31980526376fa5c214ccfead69aa7c578918f8
Layne Faler <laynefaler@gmail.com>
2018-07-23 12:14:16 -0700

    Updates to documentation for CLI

--- index.js
+++ index.js
@@ -81,6 +81,8 @@ RETRO CLI
 Convert styles from web to mobile with CSS to JSON for React Native
 
 retro documentation
+retro create
+retro generate <name-for-structured-styles-folder> <name-for-structured-styles-filename>
 retro css-to-json <source-path-to-css-file> <destintation-path-to-json>
 retro css-to-js <source-path-to-css-file> <destintation-path-to-js>
 retro js-to-json <source-path-to-js-file> <destintation-path-to-json>
@@ -88,14 +90,16 @@ retro js-to-css <source-path-to-js-file> <destintation-path-to-css>
 retro json-to-css <source-path-to-json-file> <destintation-path-to-css>
 retro json-to-js <source-path-to-json-file> <destintation-path-to-js>
 
-// Shorthands
-retro docs
-retro c2j <source-path-to-css-file> <destintation-path-to-json>
-retro c2js <source-path-to-css-file> <destintation-path-to-js>
-retro j2c <source-path-to-json-file> <destintation-path-to-css>
-retro js2c <source-path-to-js-file> <destintation-path-to-css>
-retro json2js <source-path-to-json-file> <destintation-path-to-js>
-retro js2json <source-path-to-js-file> <destintation-path-to-json>
+// Alias
+retro docs === retro documentation
+retro c === retro create
+retro g === retro generate
+retro c2j === retro css-to-json
+retro c2js === retro css-to-js
+retro j2c === retro json-to-css
+retro js2c === retro js-to-css 
+retro json2js ===  retro json-to-js
+retro js2json === retro js-to-json
 
     `)
 };
