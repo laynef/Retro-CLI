c06ec8c3d2d171f9ffda2520a07191f4420f7018
Layne Faler <laynefaler@gmail.com>
2018-07-24 10:38:06 -0700

    Updates to styles for testing

--- example/styles/index.js
+++ example/styles/index.js
@@ -1,8 +1,12 @@
 // LEAVE FOR CLI: IMPORT
+import card from './base/card';
+import bootstrap from './base/bootstrap';
 import base from './base/base';
 
 
 module.exports = {
     // LEAVE FOR CLI: EXPORT
+...card,
+...bootstrap,
 ...base,
 };
