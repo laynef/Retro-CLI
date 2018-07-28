569f576f81d725a159413b06f14d68d7687e7880
Layne Faler <laynefaler@gmail.com>
2018-07-28 11:20:17 -0700

    Fixed conversions

--- test/methods/css-to-js.js
+++ test/methods/css-to-js.js
@@ -1,13 +1,15 @@
-const fs = require('fs');
 const path = require('path');
 const { expect } = require('chai');
 const { cssToJson } = require('../../src/actions');
+const { isEmpty } = require('lodash');
 
 
 module.exports = () => {
     cssToJson(path.join(__dirname, 'conversions', 'bootstrap4.css'), path.join(__dirname, 'conversions', 'retro-bootstrap.json'));
     const json = require(path.join(__dirname, 'conversions', 'retro-bootstrap.json'));
     describe('CSS to JSON: "./conversions/retro-bootstrap.json"', () => {
-        expect(json.html)
+        it('File is not empty', () => {
+            expect(!isEmpty(json)).to.be.true
+        })
     })
 };
