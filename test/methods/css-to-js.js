6e48924f293ad1e85571adcf93ab703d411942bc
Layne Faler <laynefaler@gmail.com>
2018-07-28 11:05:38 -0700

    Updates to tests

--- test/methods/css-to-js.js
+++ test/methods/css-to-js.js
@@ -1,8 +1,13 @@
 const fs = require('fs');
 const path = require('path');
 const { expect } = require('chai');
+const { cssToJson } = require('../../src/actions');
 
 
-module.exports = (src, dest) => {
-    
+module.exports = () => {
+    cssToJson(path.join(__dirname, 'conversions', 'bootstrap4.css'), path.join(__dirname, 'conversions', 'retro-bootstrap.json'));
+    const json = require(path.join(__dirname, 'conversions', 'retro-bootstrap.json'));
+    describe('CSS to JSON: "./conversions/retro-bootstrap.json"', () => {
+        expect(json.html)
+    })
 };
