50d4f89a15e67dce1bf544c61a3c90f256ffc09b
Layne Faler <laynefaler@gmail.com>
2018-08-01 10:06:49 -0700

    Fixed an inapproapriate tab

--- src/actions/methods/generate-styles-file.js
+++ src/actions/methods/generate-styles-file.js
@@ -22,7 +22,7 @@ module.exports = {
         const importRegex = new RegExp(`// LEAVE FOR CLI: IMPORT`);
         const exportRegex = new RegExp(`// LEAVE FOR CLI: EXPORT`);
         const read = fs.readFileSync(path.join(root, 'styles', 'index.js'), { encoding: 'utf8' }).replace(importRegex, `// LEAVE FOR CLI: IMPORT
-    import ${filename} from './${base}/${filename}';`).replace(exportRegex, `// LEAVE FOR CLI: EXPORT
+import ${filename} from './${base}/${filename}';`).replace(exportRegex, `// LEAVE FOR CLI: EXPORT
     ...${filename},`);
         fs.writeFileSync(path.join(root, 'styles', 'index.js'), read);
         fs.writeFileSync(path.join(root, 'styles', base, filename + '.js'), `module.exports = {};`);
